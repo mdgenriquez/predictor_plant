33da531b71b07a635c9d239fbf8daf3da0e66ac1
Guadalupe Enriquez <122028844+mdgenriquez@users.noreply.github.com>
2025-01-27 16:52:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,13 @@ def load_data(file_path):
         st.error(f"Error al cargar el archivo CSV: {e}")
         return None
 
+
 csv_path = "base_datos_plant_smile.csv"  # Nombre del archivo
 data = load_data(csv_path)
+
+if data is not None:
+    # Validar columnas necesarias
+    if "SMILES" not in data.columns or "Name" not in data.columns:
+        st.error("El archivo CSV debe contener las columnas 'SMILES' y 'Name'.")
+    else:
+        st.title("Predictor plant")     
