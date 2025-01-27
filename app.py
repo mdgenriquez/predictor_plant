a0d211b5390996bbf1c302aeb64b96152ad3fb38
Guadalupe Enriquez <122028844+mdgenriquez@users.noreply.github.com>
2025-01-27 16:43:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
 
+def load_data(file_path):
+    try:
+        return pd.read_csv(file_path)
+    except Exception as e:
+        st.error(f"Error al cargar el archivo CSV: {e}")
+        return None
+
 csv_path = "base_datos_plant_smile.csv"  # Nombre del archivo
 data = load_data(csv_path)
